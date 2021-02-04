021f99506f491c34c6d9a680dc57dbd74b4aeb41
EC2 Default User <ec2-user@ip-172-31-21-215.ec2.internal>
2021-02-04 23:13:27 +0000

    新規投稿機能と詳細ページの追加

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,13 +1,27 @@
 class BookersController < ApplicationController
   def index
+    @bookers = Booker.all
   end
 
   def show
+    @bookers = Booker.find(params[:id])
   end
 
   def new
+    @bookers = Booker.new
+  end
+  
+  def create
+    @bookers = Booker.new(bookers_params)
+    @bookers.save
+    redirect_to bookers_path
   end
 
   def edit
+  end
+  
+  private
+  def bookers_params
+    params.require(:bookers).permit(:title, :body)
   end
 end
