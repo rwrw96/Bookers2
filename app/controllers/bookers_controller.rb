cc3d4cc475b98b37194b3b2805f8f50002d0614d
EC2 Default User <ec2-user@ip-172-31-21-215.ec2.internal>
2021-02-05 00:05:49 +0000

    投稿画面と一覧画面の統合

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -12,16 +12,19 @@ class BookersController < ApplicationController
   end
   
   def create
-    @bookers = Booker.new(bookers_params)
-    @bookers.save
-    redirect_to bookers_path
+    @bookers = Booker.new
+    if @bookers.save
+       redirect_to bookers_path , notice: "succese"
+    else
+       flash[:alert] = 'Save error!'
+    end
   end
 
   def edit
   end
   
-  private
-  def bookers_params
-    params.require(:bookers).permit(:title, :body)
-  end
+  # private
+  # def bookers_params
+  #   params.require(:bookers).permit(:title, :body)
+  # end
 end
